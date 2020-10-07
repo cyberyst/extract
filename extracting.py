0744e53e19ea6ae7739a18b1d01b6217a67ed836
cyberyst <72510463+cyberyst@users.noreply.github.com>
2020-10-07 20:33:25 +0530

    changes
    changes

---
+++ extracting.py
@@ -0,0 +1,14 @@
+# importing required modules 
+from zipfile import ZipFile 
+  
+# specifying the zip file name 
+file_name = "intershipfile.zip"
+  
+# opening the zip file in READ mode 
+zip = ZipFile(file_name, 'r')
+# printing all the contents of the zip file 
+zip.printdir() 
+# extracting all the files 
+print('Extracting all the files now...') 
+zip.extractall() 
+print('Done!') 
